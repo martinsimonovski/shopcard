0185a1a0ea7f45e8b81a12d4824249a6b8d91e16
Martin Simonovski <simonovski.martin@gmail.com>
2018-06-24 11:22:04 +0200

    new layout and controllers and buttons

---
+++ ShopCard/ViewControllers/LoginViewController.swift
@@ -0,0 +1,40 @@
+//
+//  LoginViewController.swift
+//  ShopCard
+//
+//  Created by Martin Simonovski on 6/23/18.
+//  Copyright © 2018 Martin Simonovski. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var EmailTextField: UITextField!
+    @IBOutlet weak var PasswordTextField: UITextField!
+    @IBOutlet weak var LoginBtn: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
